495b6eeaa5a13c1c4750fa683a80ce8720e93785
Jeff-victor <victorjeff.ngoyngoy@edu.ece.fr>
2026-05-27 11:23:17 +0200

    Delete passguard directory

--- passguard/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/functions.php';
-startSecureSession();
-if (!empty($_SESSION['user_id'])) {
-    auditLog($_SESSION['user_id'], 'LOGOUT');
-}
-session_destroy();
-header('Location: login.php');
-exit;
